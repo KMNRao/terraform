42268ca0a142998b5ef90e092a6e611ba7384753
KMNRao <kmohanarao09@gmail.com>
2024-02-15 14:07:12 +0530

    terraform

---
+++ locals/variables.tf
@@ -0,0 +1,18 @@
+variable "instance_names" {
+  type = list
+  default = ["mongodb","redis","mysql","rabbitmq","catalogue","user","cart","shipping","payment","dispatch","web"]
+}
+
+variable "ami_id" {
+  type = string
+  default = "ami-010c2d0fd8c55129c"
+}
+
+variable "zone_id" {
+     type = string    
+    default = "Z09009163A0GN0OVQ08D2"
+}
+
+variable "domain_name" {
+    default = "mohana.cloud"
+}
