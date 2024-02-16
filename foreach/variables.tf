87ceb54f6908ab81088d471b7b182a01fcc3695f
KMNRao <kmohanarao09@gmail.com>
2024-02-16 16:06:02 +0530

    terraform

---
+++ foreach/variables.tf
@@ -0,0 +1,30 @@
+variable "instance_names" {
+  type = map
+  default = {
+    mongodb = "t3.small"
+    redis = "t2.micro"
+    mysql = "t3.small"
+    rabbitmq = "t2.micro"
+    catalogue = "t2.micro"
+    user = "t2.micro"
+    cart = "t2.micro"
+    shipping = "t3.small"
+    payment ="t2.micro"
+    dispatch = "t2.micro"
+    web = "t2.micro"
+  }
+}
+
+variable "ami_id" {
+  type = string
+  default = "ami-010c2d0fd8c55129c"
+}
+
+variable "zone_id" {
+     type = string    
+    default = "Z09009163A0GN0OVQ08D2"
+}
+
+variable "domain_name" {
+    default = "mohana.cloud"
+}
