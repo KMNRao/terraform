8815a701ab33154252183327f72a3bdd5cb6b875
KMNRao <kmohanarao09@gmail.com>
2024-02-18 04:42:23 +0530

    terraform

--- foreach/foreach.tf
+++ foreach/foreach.tf
@@ -7,15 +7,15 @@ resource "aws_instance" "web" {
     }
 }
 
-# resource "aws_route53_record" "www" {
-#   for_each = aws_instance.web
-#   zone_id  = var.zone_id
-#   name     = "${each.key}.${var.domain_name}" #interpolation
-#   type     = "A"
-#   ttl      = 1
-#   records  = [each.key == "web" ? each.value.public_ip : each.value.private_ip]
-# }
-
-output "instance_info" {
-    value = aws_instance.web
+resource "aws_route53_record" "www" {
+  for_each = aws_instance.web
+  zone_id  = var.zone_id
+  name     = "${each.key}.${var.domain_name}" #interpolation
+  type     = "A"
+  ttl      = 1
+  records  = [each.key == "web" ? each.value.public_ip : each.value.private_ip]
 }
+
+# output "instance_info" {
+#     value = aws_instance.web
+# }
