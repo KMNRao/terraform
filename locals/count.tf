d5499fe455817decb356fb6e804ebf94813bb7ff
KMNRao <kmohanarao09@gmail.com>
2025-04-17 15:21:57 +0530

    commit

--- locals/count.tf
+++ locals/count.tf
@@ -1,8 +1,8 @@
-#resource "aws_instance" "web" {
+    resource "aws_instance" "web" {
 #     #count = 11 #count.index is a special variable given by terraform
-#     count = length(var.instance_names)
-#     ami   = var.ami_id #devops-practice
-#     instance_type = local.instance_type
+     count = length(var.instance_names)
+     ami   = var.ami_id #devops-practice
+     instance_type = local.instance_type
 #     #instance_type = var.instance_names == "MongoDB" ? "t3.small" : "t2.micro"
 #     # vpc_security_group_ids = [aws_security_group.roboshop-all.id] # this means list
 #     tags = {
@@ -10,14 +10,14 @@
 #     }
 # }
 
-# resource "aws_route53_record" "www" {
-#   count    = length(var.instance_names)
-#   zone_id  = var.zone_id
-#   name     = "${var.instance_names[count.index]}.${var.domain_name}" #interpolation
-#   type     = "A"
-#   ttl      = 1
-#   records  = [local.ip]
-# }
+    resource "aws_route53_record" "www" {
+    count    = length(var.instance_names)
+    zone_id  = var.zone_id
+    name     = "${var.instance_names[count.index]}.${var.domain_name}" #interpolation
+    type     = "A"
+    ttl      = 1
+    records  = [local.ip]
+ }
 
 
 resource "aws_instance" "web" {
